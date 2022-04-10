b293fa44be4208255a75452564de52a2d6ae2e63
Landry, Christopher <CLandry@edc.ca>
2022-04-10 16:18:26 -0400

    initial terraform structure

---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+
+key_pair_filename = "LightsailDefaultKey-ca-central-1.pem"
+
---
+++ variables.tf
@@ -0,0 +1,6 @@
+
+variable "key_pair_filename" {
+  description = "The filename of the AWS Lightsail Key Pair .pem file"
+  type        = string
+}
+
